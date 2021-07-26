8368337201b4b00e4bc00aafbc63653adeaee634
Sevrinn <sevrinnsevrinn@gmail.com>
2021-07-26 15:32:51 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  //const [tab, setTab] = useState("");
+  //const [tabList, setTabList] = useState(["Tab 1", "Tab 2", "Tab 3",]);
+  
+  return (
+    <div className="App">
+    hey
+      
+
+    </div>
+  );
+}
+
+export default App;
