5d7cac165cbacdf6bfa07b53cc40f2c57af88fd6
Sevrinn <sevrinnsevrinn@gmail.com>
2021-07-26 16:44:58 -0700

    display list of tab objects to page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 
+
 function App() {
-  //const [tab, setTab] = useState("");
-  //const [tabList, setTabList] = useState(["Tab 1", "Tab 2", "Tab 3",]);
+  const [tab, setTab] = useState("");
+  const [tabList, setTabList] = useState([
+                              {label:"Tab 1", content:"{label} content is showing here"}, 
+                              {label:"Tab 2", content:"{label} content is showing here"},
+                              {label:"Tab 3", content:"{label} content is showing here"},]);
   
   return (
     <div className="App">
-    hey
+    <h1>Tabs</h1>
+    <p>Click the tabs to see the corresponding content:)</p>
+    {
+      tabList.map((tab, index) => {
+        
+        return ( <button 
+          key={index}
+          
+          >
+          {tab.label}
+          </button>
+      );
+      })
+    }
       
 
     </div>
