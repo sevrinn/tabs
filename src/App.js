73647962afb2414065d1eed8cce1255643c49a81
Sevrinn <sevrinnsevrinn@gmail.com>
2021-07-27 12:14:10 -0700

    added display area and handleDisplayContent

--- src/App.js
+++ src/App.js
@@ -3,12 +3,33 @@ import './App.css';
 
 
 
+
+
 function App() {
   const [tab, setTab] = useState("");
   const [tabList, setTabList] = useState([
-                              {label:"Tab 1", content:"{label} content is showing here"}, 
-                              {label:"Tab 2", content:"{label} content is showing here"},
-                              {label:"Tab 3", content:"{label} content is showing here"},]);
+                              {label:"Tab 1", content: "Tab 1 content is showing here."},
+                              {label:"Tab 2", content: "Tab 2 content is showing here."},
+                              {label:"Tab 3", content: "Tab 3 content is showing here."},
+                              {label:"Tab 4", content: "Tab 4 content is showing here."}
+                              ,]);
+  const [display, setDisplay] = useState("displaying shit");
+
+  
+  // - - - - - - D I S P L A Y  C O N T E N T - - - - - - //
+  const handleDisplayContent = (e) => {
+  //  console.log("Hi there, babe. I'm correctly connected");
+  //  console.log(e);
+   tabList.map((tab, idx) => {
+     if((tab.label) === (e.target.innerText)) {
+      console.log(tab.content);
+      setDisplay(tab.content);
+     }
+     
+   })
+  }
+   
+
   
   return (
     <div className="App">
@@ -19,12 +40,15 @@ function App() {
         
         return ( <button 
           key={index}
-          
+          onClick={(e) => handleDisplayContent(e)}
           >
           {tab.label}
           </button>
       );
       })
+    }
+    {
+      <div className="contentDisplayArea">{display}</div>
     }
       
 
